0c17ed4174d309befa069d0757702ce75ac36110
Gwyn-Dayot <20230028015@my.xu.edu.ph>
2025-04-25 22:38:30 +0800

    Initial commit - TicTacToe

---
+++ GameBoard.java
@@ -0,0 +1,56 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.border.Border;
+
+public class GameBoard extends Frame {
+    private Button[][] buttons = new Button[3][3];
+    private Label sLabel;
+    private Button rButton;
+    private GameController ctrl;
+
+    public GameBoard() {
+        super("AWT TicTacToe");
+        setLayout(new BorderLayout());
+
+        sLabel = new Label("Player 1's turn");
+        add(sLabel, BorderLayout.NORTH);
+
+        Panel gridPanel = new Panel(new GridLayout(3, 3));
+        Font font = new Font("Arial", Font.BOLD, 60);
+
+        ctrl = new GameController(this);
+        
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                buttons[row][col] = new Button(" ");
+                buttons[row][col].setFont(font);
+                buttons[row][col].addActionListener(ctrl);
+                gridPanel.add(buttons[row][col]);
+            }
+        }
+
+        add(gridPanel, BorderLayout.CENTER);
+
+        Panel bottomPanel = new Panel();
+        rButton = new Button("New Game");
+        rButton.addActionListener(e -> ctrl.resetGame());
+        bottomPanel.add(rButton);
+        add(bottomPanel, BorderLayout.SOUTH);
+
+        setSize(400, 450);
+        setVisible(true);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                dispose();
+            }
+        });
+    }
+
+    public Button[][] getButtons() {
+        return buttons;
+    }
+
+    public void updateStatus(String text){
+        sLabel.setText(text);
+    }
+}
